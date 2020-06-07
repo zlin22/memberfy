6367d285e51d84a772b8edcb7353d3f36e214022
Zhi Lin <zhi.long.lin@gmail.com>
2020-06-07 03:38:41 -0400

    django rest tutorial 4

--- membership/urls.py
+++ membership/urls.py
@@ -1,14 +1,19 @@
 from django.urls import include, path
-from rest_framework import routers
+# from rest_framework import routers
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
